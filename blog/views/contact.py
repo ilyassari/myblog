e96efed0262923d00622fecf0ea2d37855b787ff
İlyas Sarı <ilyassari@mail.com>
2021-06-07 12:06:38 +0300

    post detail page designed with bootstrap

--- blog/views/contact.py
+++ blog/views/contact.py
@@ -2,10 +2,14 @@ from django.shortcuts import render
 from django.core.paginator import Paginator
 from django.db.models import Q
 
+from blog.models import ContactModel, BannerModel
+
 def contact(request):
+    banner = BannerModel.objects.filter(active='True')
     context={
-    'key' : 'value',
-    'key1' : 'value1',
-    'key2' : 'value2',
+        'key' : 'value',
+        'key1' : 'value1',
+        'key2' : 'value2',
+        'banners' : banner,
     }
     return render(request, 'contact.html', context)
--- blog/views/post.py
+++ blog/views/post.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from django.db.models import Q
 
-from blog.models import PostModel, BannerModel
+from blog.models import PostModel, CommentModel, BannerModel
 
 def post(request, slug):
     post = get_object_or_404(PostModel, slug=slug)
+    comments = post.p_comments.all()
     banner = BannerModel.objects.filter(active='True')
+    print(comments)
     context={
-    'key' : 'value',
-    'key1' : 'value1',
-    'key2' : 'value2',
+        'post' : post,
+        'comments' : comments,
+        'banners' : banner,
     }
-    return render(request, 'blog.html', context)
+    return render(request, 'one_post.html', context)
